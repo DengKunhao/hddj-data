5a0d376e51fce29e64710df5946b10e4bb00d340
DengKunhao <50043427+DengKunhao@users.noreply.github.com>
2021-04-26 17:30:22 +0800

    Add files via upload

---
+++ index_matching_page/js/bottom-logo.js
@@ -0,0 +1,4 @@
+document.write(unescape("%3Cspan id='_ideConac' %3E%3C/span%3E"));
+var span_msg = document.getElementById("_ideConac");
+span_msg.innerHTML =
+	'<a href="img/red.png" target="_blank"><img id="imgConac" vspace="0" hspace="0" border="0" src="img/red.png" data-bd-imgshare-binded="1"></a>';
